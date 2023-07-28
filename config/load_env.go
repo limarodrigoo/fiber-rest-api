a62d70c85b5600620749446db68389dd02cc28ad
Rodrigo Lima <limarodrigoo@outlook.com>
2023-07-28 14:54:32 -0300

    🔧 create config file

---
+++ config/load_env.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	DatabaseUser       string `mapstructure:"DB_USER"`
+	DatabasePassword   string `mapstructure:"DB_PASSWORD"`
+	DatabaseName       string `mapstructure:"DB_NAME"`
+	DatabaseCollection string `mapstructure:"DB_COLLECTION"`
+	DatabasePort       string `mapstructure:"DB_PORT"`
+	DatabaseHost       string `mapstructure:"DB_HOST"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigType("env")
+	viper.SetConfigName("app")
+
+	viper.AutomaticEnv()
+
+	if err = viper.ReadInConfig(); err != nil {
+		panic(fmt.Errorf("fatal error config file: %w", err))
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
