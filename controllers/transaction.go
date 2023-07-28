be6eb07dadfa2cd758914ebadfee3775d7b144a8
Rodrigo Lima <limarodrigoo@outlook.com>
2023-07-28 14:57:14 -0300

    ✨ create transaction controller

---
+++ controllers/transaction.go
@@ -0,0 +1,139 @@
+package controllers
+
+import (
+	"fmt"
+	"rest-api/helper"
+	"rest-api/models"
+	"rest-api/repository"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type TransactionController interface {
+	FindAllTransactions(ctx *fiber.Ctx) error
+	FindTransactionByHash(ctx *fiber.Ctx) error
+	FindTransactionByBlock(ctx *fiber.Ctx) error
+	FindTransactionByAddress(ctx *fiber.Ctx) error
+	DeleteTransaction(ctx *fiber.Ctx) error
+	Update(ctx *fiber.Ctx) error
+}
+
+type transactionController struct {
+	transactionRepo repository.TransactionRepository
+	blocksRepo      repository.BlockRepository
+	addresRepo      repository.AddressRepository
+}
+
+func NewTransactionsController(transactionRepo repository.TransactionRepository, blocksRepo repository.BlockRepository, addresRepo repository.AddressRepository) TransactionController {
+	return &transactionController{transactionRepo, blocksRepo, addresRepo}
+}
+
+func (t *transactionController) DeleteTransaction(ctx *fiber.Ctx) error {
+	hash := ctx.Params("hash")
+	err := t.transactionRepo.DeleteTransaction(hash)
+	if err != nil {
+		fmt.Println(err)
+		return ctx.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
+		"deleted": true,
+	})
+}
+
+func (t *transactionController) FindAllTransactions(ctx *fiber.Ctx) error {
+	result, err := t.transactionRepo.GetAllTransactions()
+
+	if err != nil {
+		fmt.Println(err)
+		return ctx.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
+		"data": result,
+	})
+}
+
+func (t *transactionController) FindTransactionByHash(ctx *fiber.Ctx) error {
+	var err error
+	var result *models.Transaction
+	hash := ctx.Params("hash")
+	result, err = t.transactionRepo.GetTransactionByHash(hash)
+
+	if err != nil {
+		result, err := helper.GetTransactionJSON(hash)
+		if err != nil {
+			fmt.Println(err)
+			return ctx.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+		t.transactionRepo.SaveTransaction(*result)
+		return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
+			"data": result,
+		})
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
+		"data": result,
+	})
+}
+
+func (t *transactionController) FindTransactionByBlock(ctx *fiber.Ctx) error {
+	blockHash := ctx.Params("hash")
+	block, err := t.blocksRepo.GetByHash(blockHash)
+
+	if err != nil {
+		result, err := helper.GetBlockJSON(blockHash)
+		if err != nil {
+			fmt.Println(err)
+			return ctx.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+		t.blocksRepo.Save(*result)
+		t.transactionRepo.BulkSaveTransaction(result.Tx)
+		return ctx.JSON(fiber.Map{
+			"data": result.Tx,
+		})
+	} else {
+		return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
+			"data": block.Tx,
+		})
+	}
+}
+
+func (t *transactionController) FindTransactionByAddress(ctx *fiber.Ctx) error {
+	addressHash := ctx.Params("address")
+	address, err := t.addresRepo.GetByAddress(addressHash)
+
+	if err != nil {
+		result, err := helper.GetAddressJSON(addressHash)
+
+		if err != nil {
+			fmt.Println(err)
+			return ctx.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+		t.addresRepo.Save(*result)
+		t.transactionRepo.BulkSaveTransaction(result.Tx)
+
+		return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
+			"data": result.Tx,
+		})
+	} else {
+		return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
+			"data": address.Tx,
+		})
+	}
+
+}
+
+func (t *transactionController) Update(ctx *fiber.Ctx) error {
+	panic("unimplemented")
+}
