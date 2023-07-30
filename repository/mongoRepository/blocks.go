f4c104f23d794cf9ee31b7c96ef0720b726211e5
Rodrigo Lima <limarodrigoo@outlook.com>
2023-07-30 18:02:06 -0300

    ♻️ refactor repository folder

---
+++ repository/mongoRepository/blocks.go
@@ -0,0 +1,79 @@
+package mongorepository
+
+import (
+	"context"
+	"rest-api/helper"
+	"rest-api/models"
+	"rest-api/repository"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+const BlocksCollection = "blocks"
+
+type blockRepository struct {
+	c *mongo.Collection
+}
+
+func NewBlocksRepository(conn mongo.Database) repository.BlockRepository {
+	return &blockRepository{conn.Collection(BlocksCollection)}
+}
+
+func (b *blockRepository) Delete(id string) error {
+	var filter = bson.D{{Key: "hash", Value: id}}
+
+	_, err := b.c.DeleteOne(context.TODO(), filter)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (b *blockRepository) GetAll() (block []*models.Block, err error) {
+	filter := bson.D{}
+
+	cursor, err := b.c.Find(context.TODO(), filter)
+
+	if err != nil {
+		helper.ErrorPanic(err)
+	}
+
+	var blocks []*models.Block
+	err = cursor.All(context.TODO(), &blocks)
+
+	for cursor.Next(context.TODO()) {
+		var block *models.Block
+		cursor.Decode(&block)
+		blocks = append(blocks, block)
+	}
+
+	return blocks, err
+}
+
+func (b *blockRepository) GetByHash(hash string) (block *models.Block, err error) {
+
+	var filter = bson.D{{Key: "hash", Value: hash}}
+
+	var blockFound *models.Block
+
+	err = b.c.FindOne(context.TODO(), filter).Decode(&blockFound)
+
+	return blockFound, err
+}
+
+func (b *blockRepository) Save(block models.Block) error {
+	_, err := b.c.InsertOne(context.TODO(), block)
+
+	if err != nil {
+		helper.ErrorPanic(err)
+	}
+
+	return nil
+}
+
+func (b *blockRepository) Update(block *models.Block) error {
+	panic("unimplemented")
+}
---
+++ repository/mongoRepository/transaction_test.go
@@ -0,0 +1,134 @@
+package mongorepository
+
+import (
+	"fmt"
+	"os"
+	"rest-api/models"
+	"rest-api/tests/testdata"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/integration/mtest"
+)
+
+func TestTransactionFindByHashRepository(t *testing.T) {
+	databaseName := "cryptos_test"
+	err := os.Setenv("DB_NAME", databaseName)
+	if err != nil {
+		t.FailNow()
+		return
+	}
+	defer os.Clearenv()
+
+	mtestdb := mtest.New(t, mtest.NewOptions().ClientType(mtest.Mock))
+	defer mtestdb.Close()
+
+	mtestdb.Run("when_sending_a_valid_hash_returns_success", func(mt *mtest.T) {
+
+		transactionData := testdata.GetTransactionMongoData()
+		mt.AddMockResponses(mtest.CreateCursorResponse(
+			1,
+			fmt.Sprintf("%s.%s", databaseName, "transactions"),
+			mtest.FirstBatch,
+			convertEntityToBson(transactionData),
+		))
+		databaseMock := mt.Client.Database(databaseName)
+
+		repo := NewTransactionsRepository(*databaseMock)
+
+		transaction, err := repo.GetTransactionByHash(transactionData.Hash)
+
+		assert.Nil(t, err)
+		assert.EqualValues(t, transactionData.Hash, transaction.Hash)
+		assert.EqualValues(t, transactionData.TxIndex, transaction.TxIndex)
+		assert.EqualValues(t, transactionData.BlockIndex, transaction.BlockIndex)
+	})
+
+	mtestdb.Run("when_returns_no_document_found", func(mt *mtest.T) {
+		mt.AddMockResponses(mtest.CreateCursorResponse(
+			0,
+			fmt.Sprintf("%s.%s", databaseName, "transactions"),
+			mtest.FirstBatch,
+		))
+
+		databaseMock := mt.Client.Database(databaseName)
+
+		repo := NewTransactionsRepository(*databaseMock)
+
+		transaction, err := repo.GetTransactionByHash("test")
+
+		assert.NotNil(t, err)
+		assert.Equal(t, err.Error(), "mongo: no documents in result")
+		assert.Nil(t, transaction)
+	})
+
+	mtestdb.Run("when_mongo_db_returns_error", func(mt *mtest.T) {
+		mt.AddMockResponses(bson.D{
+			{Key: "error", Value: 0},
+		})
+
+		databaseMock := mt.Client.Database(databaseName)
+
+		repo := NewTransactionsRepository(*databaseMock)
+
+		transaction, err := repo.GetTransactionByHash("test")
+
+		assert.Nil(t, transaction)
+		assert.NotNil(t, err)
+	})
+}
+
+func TestDeleteTransactionRepository(t *testing.T) {
+	databaseName := "cryptos_test"
+	err := os.Setenv("DB_NAME", databaseName)
+	if err != nil {
+		t.FailNow()
+		return
+	}
+	defer os.Clearenv()
+
+	mtestdb := mtest.New(t, mtest.NewOptions().ClientType(mtest.Mock))
+	defer mtestdb.Close()
+
+	mtestdb.Run("when_delete_is_successfull", func(mt *mtest.T) {
+		mt.AddMockResponses(bson.D{
+			{Key: "ok", Value: 1},
+			{Key: "n", Value: 1},
+			{Key: "acknowledged", Value: true},
+		})
+
+		databaseMock := mt.Client.Database(databaseName)
+
+		repo := NewTransactionsRepository(*databaseMock)
+
+		err := repo.DeleteTransaction("test")
+
+		assert.Nil(t, err)
+	})
+
+	mtestdb.Run("when_delete_is_not_successfull", func(mt *mtest.T) {
+		mt.AddMockResponses(bson.D{
+			{Key: "ok", Value: 0},
+			{Key: "n", Value: 1},
+			{Key: "acknowledged", Value: false},
+		})
+
+		databaseMock := mt.Client.Database(databaseName)
+
+		repo := NewTransactionsRepository(*databaseMock)
+
+		err := repo.DeleteTransaction("test")
+
+		assert.NotNil(t, err)
+	})
+}
+
+func convertEntityToBson(transaction models.TransactionMongoDB) bson.D {
+	return bson.D{
+		{Key: "_id", Value: transaction.ID},
+		{Key: "hash", Value: transaction.Hash},
+		{Key: "tx_index", Value: transaction.TxIndex},
+		{Key: "block_index", Value: transaction.BlockIndex},
+	}
+}
